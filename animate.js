45d84ebb4f55eaa339cd832771467127dc385096
Moshe <mobgadol@gmail.com>
2022-11-21 23:22:06 -0500

    added animation for small screen size

--- animate.js
+++ animate.js
@@ -9,3 +9,10 @@ mm.add("(min-width: 700px)", () => {
   .from(".feature-products .prod-title", { opacity: 0, stagger: 0.5 })
   .from(".feature-products li", { x: "200vw", ease: "power2", stagger: 0.3, duration: 2 }); 
 });
+
+mm.add("(max-width: 699px)", () => {
+    timeline
+   .from("header", { y: "-100", ease: "bounce", duration: 1.5})
+   .from(".main-content", { y: "200vh", ease: "elastic", duration: 2 })
+   .from("header ul li", { opacity: 0, stagger: 0.5 });
+});
