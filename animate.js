17f4c8d27641459e517f07c3c577d5a39820474c
Moshe <mobgadol@gmail.com>
2022-11-21 22:42:38 -0500

    added resposnive behaviour to the page

--- animate.js
+++ animate.js
@@ -9,5 +9,3 @@ mm.add("(min-width: 700px)", () => {
   .from(".feature-products .prod-title", { opacity: 0, stagger: 0.5 })
   .from(".feature-products li", { x: "200vw", ease: "power2", stagger: 0.3, duration: 2 }); 
 });
-
-
